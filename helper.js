38db9922a3e63413bc31eb978e4b4468a608ee09
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-10-03 22:37:35 -0400

    10 Dundas 2.0 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -1,5 +1,45 @@
+function get_instagram(url, total, size, callback){
+    var html = '<div class="insta ih-item circle effect19"><a class="ig-image" target="_blank" href="{{{link}}}"><img src="{{{image}}}" alt="{{caption}}"/><div class="info"><div class="content"><p>{{{caption_short}}}</p></div></div></a></div>'
+    var item_rendered = [];
+    Mustache.parse(html); 
+    log('fetching instagram data from: ' + url);
+    $.getJSON(url).done(function(data) {
+        var insta_feed = data.social.instagram
+        if(insta_feed != null){
+            main_feed = insta_feed.splice(1,12);
+            $.each(main_feed, function(i,v){
+                var feed_obj = {}
+                if(v.caption != null){
+                    if(v.caption.text.length > 100){
+                        feed_obj.caption_short = v.caption.text.substring(0,99) + "...";
+                    }
+                    // feed_obj.caption = v.caption.text
+                }
+                else{
+                    feed_obj.caption = ""
+                }
+                feed_obj.image = v.images[size].url
+                feed_obj.link = v.link
+                if (i < total){
+                    
+                    var ig_rendered =  Mustache.render(html,feed_obj);
+                    item_rendered.push(ig_rendered.trim());
+                }
+            })
+            callback(item_rendered.join(''))
+        }
+    });
+}
+
+function render_instagram_single(data){
+    $('.instafeed_single').html(data)
+}
+
+function render_instagram(data){
+    $('#instafeed').html(data)
+}
 function init() {
-    
+    get_instagram("//thegateway.mallmaverick.com/api/v3/thegateway/social.json", 12, 'standard_resolution', render_instagram);   
 }
 
 function show_content() {
