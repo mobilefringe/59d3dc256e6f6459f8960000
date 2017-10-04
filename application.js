1d1ee1157ef9d6025035d04ab9b7af240f671fab
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-10-04 16:13:39 -0400

    10 Dundas 2.0 1.0  /application.js

--- application.js
+++ application.js
@@ -1,3 +1,67 @@
+function renderEvents(container, template, collection){
+    var mall_name = getPropertyDetails().name;
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html); 
+    $.each( collection , function( key, val ) {
+        val.store_name = "10 Dundas";
+        val.image_url = val.event_image_url_abs;
+        val.logo  = default_image.image_url;
+        if(val.image_url.indexOf('missing.png') > 0){
+            val.image_url  = val.logo;
+        }
+        
+        if (val.name.length > 30){
+            val.name_short = val.name.substring(0,30) + "...";
+        } else {
+            val.name_short = val.name;
+        }
+        
+        var show_date = moment(val.show_on_web_date);
+        var start = moment(val.start_date).tz(getPropertyTimeZone());
+        var end = moment(val.end_date).tz(getPropertyTimeZone());
+        if (start.format("DMY") == end.format("DMY")){
+            val.dates = start.format("MMM D")
+        } else {
+            val.dates = start.format("MMM D") + " - " + end.format("MMM D")
+        }
+        
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+    });
+    $(container).html(item_rendered.join(''));
+}
+
+function renderEventDetails(container, template, collection){
+    var mall_name = getPropertyDetails().name;
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html); 
+    item_list.push(collection);
+    $.each( item_list , function( key, val ) {
+        val.image_url = getImageURL(val.event_image_url);
+        if(val.image_url.indexOf('missing.png') > 0){
+            val.image_show = "display:none";
+        }
+        
+        var show_date = moment(val.show_on_web_date);
+        var start = moment(val.start_date).tz(getPropertyTimeZone());
+        var end = moment(val.end_date).tz(getPropertyTimeZone());
+        if (start.format("DMY") == end.format("DMY")){
+            val.dates = start.format("MMM D")
+        }
+        else{
+            val.dates = start.format("MMM D") + " - " + end.format("MMM D")
+        }
+        
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+    });
+    $(container).html(item_rendered.join(''));
+}
+
 function renderHomeHours(container, template, collection){
     var item_list = [];
     var item_rendered = [];
