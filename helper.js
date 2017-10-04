c815342da3cc11f30ae45ca7ff0f1ac4a5a95c62
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-10-03 21:30:24 -0400

    10 Dundas 2.0 1.0  /helper.js

---
+++ helper.js
@@ -0,0 +1,20 @@
+function init() {
+    var hours = getPropertyRegularHours();
+    var hours_mf = [];
+    var hours_sat = [];
+    var hours_sun = [];
+    $.each(hours, function(key, val){
+        if (val.day_of_week == 1 && val.is_holiday == false){
+            hours_mf.push(val);
+        }
+        if (val.day_of_week == 6 && val.is_holiday == false){
+            hours_sat.push(val);
+        }
+        if (val.day_of_week == 0 && val.is_holiday == false){
+            hours_sun.push(val);
+        }
+    });
+    renderHours('#hours_mf_container','#hours_mf_template', hours_mf, 'reg_hours');
+    renderHours('#hours_sat_container','#hours_sat_template', hours_sat, 'reg_hours');
+    renderHours('#hours_sun_container','#hours_sun_template', hours_sun, 'reg_hours');
+}
