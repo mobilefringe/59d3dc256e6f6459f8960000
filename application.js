0ada0df204c0c927f0a264ceea38d3c8526f6c6f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-10-05 12:42:45 -0400

    10 Dundas 2.0 1.0  /application.js

--- application.js
+++ application.js
@@ -407,7 +407,7 @@ function renderStoreDetails(container, template, collection, slug){
         } else {
             val.coming_soon_store = "display: none;"
         }
-        val.category_list = getCategoriesNamesByStoreSlug(slug);
+        // val.category_list = getCategoriesNamesByStoreSlug(slug);
         val.map_x_coordinate = val.x_coordinate - 19;
         val.map_y_coordinate = val.y_coordinate - 58;
         val.property_map = getPropertyDetails().mm_host + getPropertyDetails().map_url;
