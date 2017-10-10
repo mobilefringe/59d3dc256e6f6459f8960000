6561ba3dd10be0804a6279f56ef9c46b1b47176c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-10-10 15:58:01 -0400

    10 Dundas 2.0 1.0  /application.js

--- application.js
+++ application.js
@@ -1,3 +1,36 @@
+function renderBanner(banner_template,home_banner,banners){
+    var item_list = [];
+    var item_rendered = [];
+    var banner_template_html = $(banner_template).html();
+    Mustache.parse(banner_template_html);   // optional, speeds up future uses
+    $.each( banners , function( key, val ) {
+        today = new Date();
+        start = new Date (val.start_date);
+        start.setDate(start.getDate());
+        if(val.url == "" || val.url === null){
+            val.css = "style=cursor:default;";
+            val.noLink = "return false";
+        }
+        if (start <= today){
+            if (val.end_date){
+                end = new Date (val.end_date);
+                end.setDate(end.getDate() + 1);
+                if (end >= today){
+                    item_list.push(val);  
+                }
+            } else {
+                item_list.push(val);
+            }
+        }
+    });
+
+    $.each( item_list , function( key, val ) {
+        var repo_rendered = Mustache.render(banner_template_html,val);
+        item_rendered.push(repo_rendered);
+    });
+    $(home_banner).html(item_rendered.join(''));
+}
+
 function renderEvents(container, template, collection){
     var mall_name = getPropertyDetails().name;
     var item_list = [];
