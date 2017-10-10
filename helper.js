5cacadd3c87e284a8fba5a8c5abcc8d6c2c1f12c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-10-10 13:40:06 -0400

    10 Dundas 2.0 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -68,6 +68,8 @@ function render_instagram(data){
     $('#instafeed').html(data)
 }
 function init() {
+    store_search();
+    
     get_instagram("//thegateway.mallmaverick.com/api/v3/thegateway/social.json", 12, 'standard_resolution', render_instagram);   
 }
 
@@ -126,4 +128,39 @@ function show_content() {
     renderHours('#hours_mf_container','#hours_mf_template', hours_mf, 'reg_hours');
     renderHours('#hours_sat_container','#hours_sat_template', hours_sat, 'reg_hours');
     renderHours('#hours_sun_container','#hours_sun_template', hours_sun, 'reg_hours');
+}
+
+function store_search() {
+    $('#close_search_results').click(function(){
+        $(this).hide();
+        $('#store_search_img').show();
+        $('#store_search_results').html('');
+        $('#store_search_results').hide();
+        $('#store_search').val('')
+    });
+    $('#store_search').keyup(function(){
+        if ($('#store_search').val() == ""){
+            $('#store_search_results').html('');
+            $('#store_search_results').hide();
+            $('#close_search_results').hide();
+        } else {
+            $('#store_search_img').hide();
+            $('#close_search_results').show();
+            $('#store_search_results').html('');
+            
+            var val = $(this).val().toLowerCase();
+            var results = getSearchResults(val);
+            var s_stores = results.stores;
+            
+            if(s_stores != undefined && s_stores.length > 0){
+                $.each(s_stores, function(i, v){
+                    var div_stores = "<div class='store_search_list'>";
+                    div_stores = div_stores + "<h4><a href='/stores/" + v.slug + "'>" + v.name + "</a></h4>";
+                    div_stores = div_stores + "</div>";
+                    $('#store_search_results').append(div_stores);
+                    $('#store_search_results').show();
+                });
+            }
+        }
+    });
 }
