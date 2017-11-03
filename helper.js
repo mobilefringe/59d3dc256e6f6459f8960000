fbec5f81def1e06778cd9f545fd4da20d3d0372d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-03 10:31:54 -0400

    10 Dundas 2.0 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -110,6 +110,9 @@ function show_cat_stores(){
 }
 
 function show_content() {
+    $('.yield').fadeIn();
+    $(".modal-backdrop").remove();
+    
     var today_hours = getTodaysHours();
     renderHomeHours('#home_hours_container', '#home_hours_template', today_hours);
     renderHomeHours('#home_hours_container_footer', '#home_hours_template_footer', today_hours)
