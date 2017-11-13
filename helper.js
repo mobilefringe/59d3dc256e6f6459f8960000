7da8278947f065db082fbb60f985f6902b5a6767
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-13 11:02:11 -0500

    10 Dundas 2.0 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -132,7 +132,7 @@ function show_cat_stores(){
             $('#cat_name').hide();   
             var initial = $(".store_initial").val();
             console.log(initial)
-            if(initial == null){
+            if($(".store_initial").val() == null){
                 $(".store_initial").css("display", "none");
             }
         }
