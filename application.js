d0fcf0697e3f0e686f77eb8c986795e073582707
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-15 15:15:06 -0500

    10 Dundas 2.0 1.0  /application.js

--- application.js
+++ application.js
@@ -110,6 +110,22 @@ function renderEventDetails(container, template, collection){
     $(container).html(item_rendered.join(''));
 }
 
+function renderFeatureItems(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html); 
+    $.each( collection , function( key, val ) {
+        if(val.url == "" || val.url === null){
+           val.css = "style=cursor:default;";
+           val.noLink = "return false";
+        }
+        var repo_rendered = Mustache.render(template_html,val);
+        item_rendered.push(repo_rendered);
+    });
+    $(container).html(item_rendered.join(''));
+}
+
 function renderGeneral(container, template, collection){
     var item_list = [];
     var item_rendered = [];
