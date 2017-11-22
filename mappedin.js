36b5409bb8e879b4a109ccca535b4c2e05a6f9d7
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-22 17:40:39 -0500

    10 Dundas 2.0 1.0  /mappedin.js

--- mappedin.js
+++ mappedin.js
@@ -97,7 +97,7 @@ function hightlightStore(name) {
 	$.each( locations , function( key, val ) {
         var location_name = (val.name).toLowerCase();
         console.log(location_name)
-	    if((store_details_name).equals(location_name)){
+	    if(store_details_name == location_name){
 	        console.log(location_name)
 	        store_details_location.push(val);
         } 
