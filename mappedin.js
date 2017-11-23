8e068e7450a7b3c978f8ad85338bb4d8feba8fe0
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-23 11:48:07 -0500

    10 Dundas 2.0 1.0  /mappedin.js

--- mappedin.js
+++ mappedin.js
@@ -91,7 +91,7 @@ function getRandomInArray(array) {
 
 function hightlightStore(name) {
     var store_details_name = name.toLowerCase();
-    console.log(store_details_name)
+    console.log("Store Name: " + store_details_name);
     var locations = polygonedLocations;
     var store_details_location = [];
 	$.each( locations , function( key, val ) {
